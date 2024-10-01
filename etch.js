8dada00b35d9d62b55c8a08309366c537098be26
Matthias Hochmuth <deveolpment@matthias-hochmuth.de>
2024-10-01 09:07:49 +0200

    restart button added

--- etch.js
+++ etch.js
@@ -1,5 +1,42 @@
+function prepareButton() {
+    const restartButton = document.querySelector("#restartButton");
+    restartButton.addEventListener("click", userFieldInput);
+}
+
+function userFieldInput() {
+    let fieldSize = prompt("Please enter the dimensions of the new field (1 - 100)");
+    if (validateInput(fieldSize)) {
+        createArea(fieldSize);
+    } else {
+        alert("Invalid value entered - please try again");
+        userFieldInput();
+    }
+}
+
+function validateInput(userInput) {
+    let isValid = true;
+
+    if (userInput == null || userInput == "") {
+        isValid = false;
+    } else {
+        userInput = parseInt(userInput);
+
+        if (userInput == "NaN") {
+            isValid = false;
+        } else {
+            if((userInput < 1) || (userInput > 100)){
+                isValid = false;
+            }
+        }
+    }
+
+    return isValid;
+}
+
 function createArea(numberOfRows) {
     const scratchArea = document.querySelector("#scratchArea");
+
+    scratchArea.innerHTML = "";
 
     for (let i = 0; i < numberOfRows; i++) {
         scratchArea.appendChild(createRow(numberOfRows));
@@ -7,7 +44,6 @@ function createArea(numberOfRows) {
 }
 
 function createRow(numberOfFields) {
-
     const scratchRow = document.createElement("div");
     scratchRow.classList.add("scratchRow");
 
@@ -24,9 +60,10 @@ function createField() {
 
     scratchField.addEventListener("mouseenter", () => {
         scratchField.classList.add("scratchedField");
-      });
+    });
 
     return scratchField;
 }
 
+prepareButton();
 createArea(16);
